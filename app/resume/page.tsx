72349fee4ac09934200fdf35afda116faff2841d
Mit Sheth <88057396+MitSheth7@users.noreply.github.com>
2023-09-20 13:38:12 -0400

    Update page.tsx

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -5,7 +5,7 @@ const Resume = () => {
     <div>
       <iframe
         className="w-full h-[100vh] mb-6 rounded-lg block border focus:ring-gray-300 focus:border-gray-500 border-gray-300 dark:border-gray-600"
-        src="https://docs.google.com/document/d/e/2PACX-1vSoY1pgbo4O5qixhL3fyF4LC2cMVDg-ge3rjuZUf26tSNtzdxmsXb3E_o4gFjnOOPFF-PYGsDLgl7sO/pub?embedded=true"
+        src="https://drive.google.com/uc?export=view&id=1u3bQhjTuS1MRDK7v6BZs5Prr6NwWfsFm"
       />
     </div>
   );
