939333c73fbe2cd33a6335f8a8db34f3e65ae10f
Mit Sheth <88057396+MitSheth7@users.noreply.github.com>
2024-05-13 10:30:17 -0400

    Update page.tsx

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -5,7 +5,7 @@ const Resume = () => {
     <div>
       <iframe
         className="w-full h-[100vh] mb-6 rounded-lg block border focus:ring-gray-300 focus:border-gray-500 border-gray-300 dark:border-gray-600"
-        src="https://drive.google.com/uc?export=view&id=1ooAXtyxdl93ompaZjFe37nAsjOBlAHyf"
+        src="https://drive.google.com/uc?export=view&id=1XDTH-K-T-QXdj86k8J6aYMfdejUXOrs8"
       />
     </div>
   );
